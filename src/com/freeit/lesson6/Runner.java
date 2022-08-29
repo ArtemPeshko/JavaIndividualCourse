56bc89949e332989e5923a3695603059e17af64b
Artem Peshko <artem.peshko@alseda.by>
2022-08-29 21:12:34 +0300

    init commit

--- src/com/freeit/lesson6/Runner.java
+++ src/com/freeit/lesson6/Runner.java
@@ -2,7 +2,6 @@ package com.freeit.lesson6;
 
 import java.util.Date;
 import java.util.Scanner;
-import java.util.UUID;
 
 /**
  * Created by Artem Peshko on 22.08.2022
@@ -10,44 +9,50 @@ import java.util.UUID;
  * E-Mail artem.peshko@gmail.com
  */
 public class Runner {
+    static String name;
+    static String description;
+    static String signature;
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-//        Note[] noteBooks = new Note[3];
-//
-//        Scanner scanner = new Scanner(System.in);
-//        String name;
-//        String description;
-//        String signature;
-//
-//        for (int i = 0; i < noteBooks.length; i++) {
-//            System.out.println("Введите называние записи " + (i + 1));
-//            name = scanner.nextLine();
-//            System.out.println("Введите описание записи " + (i + 1));
-//            description = scanner.nextLine();
-//            System.out.println("Введите подпись " + (i + 1));
-//            signature = scanner.nextLine();
-//
-//            noteBooks[i] = new Note(name,
-//                    new Date(),
-//                    description,
-//                    signature);
-//        }
-//        for (int i = 0; i < noteBooks.length; i++) {
-//            System.out.println(noteBooks[i]);
-//        }
-//
-//        System.out.println("Какую запись изменить");
-//        int index = scanner.nextInt();
-//        System.out.println("Введите называние записи " );
-//        name = scanner.nextLine();
-//        System.out.println("Введите описание записи " );
-//        description = scanner.nextLine();
-//        System.out.println("Введите подпись " );
-//        signature = scanner.nextLine();
-//
-//        noteBooks[index] = new Note(name, new Date(), description, signature);
-//
-//        for (int i = 0; i < noteBooks.length; i++) {
-//            System.out.println(noteBooks[i]);
-//        }
+        Note[] noteBooks = new Note[3];
+        fillArray(noteBooks);
+        printArray(noteBooks);
+        changeNote(noteBooks);
+        printArray(noteBooks);
+    }
+
+    private static void fillArray(Note[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Введите называние записи " + (i + 1));
+            name = scanner.nextLine();
+            System.out.println("Введите описание записи " + (i + 1));
+            description = scanner.nextLine();
+            System.out.println("Введите подпись " + (i + 1));
+            signature = scanner.nextLine();
+            array[i] = createNote(name, new Date(), description, signature);
+        }
+    }
+
+    private static void changeNote(Note[] array) {
+        System.out.println("Какую запись изменить");
+        int index = scanner.nextInt();
+        System.out.println("Введите называние записи ");
+        name = scanner.nextLine();
+        System.out.println("Введите описание записи ");
+        description = scanner.nextLine();
+        System.out.println("Введите подпись ");
+        signature = scanner.nextLine();
+        array[index - 1] = createNote(name, new Date(), description, signature);
+    }
+
+    private static Note createNote(String name, Date createdDate, String description, String signature) {
+        return new Note(name, createdDate, description, signature);
+    }
+
+    private static void printArray(Note[] array) {
+        for (Note noteBook : array) {
+            System.out.println(noteBook);
+        }
     }
 }
